e74b6327b2b68b9efc9cfd55fdcdec18a992ee94
Firdous Jahan Begum <juveriyafirdous2006@gmail.com>
2026-06-22 12:21:27 +0530

    Update database.py

--- services/database.py
+++ services/database.py
@@ -1,21 +1,25 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_FILE = "vectoralign.db"
 DATABASE_URL = f"sqlite:///{DB_FILE}"
 
-# Connect and configure
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Define your User model
 class User(Base):
-    _tablename_ = "users"
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
 
+class Scan(Base):
+    __tablename__ = "scans"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, nullable=False)
+    ats_score = Column(Float, nullable=False)
+
 def init_db():
-    # This creates the tables based on your defined models
+    # This will create all tables defined in Base (users and scans)
     Base.metadata.create_all(bind=engine)
